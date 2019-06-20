1a1a4923aa7ef663f0358c761c781aa3ac31c3b9
Rashard Pleasant <rashard.pleasant@gmail.com>
2019-06-20 22:38:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,22 @@
-counter = 0
-until counter == 20
+# counter = 0
+# until counter == 20
+#   puts "#{number} SECOND(S)!"
+#   counter += 1
+# end
+
+def countdown(number)
+  until number == 0 
   puts "#{number} SECOND(S)!"
-  counter += 1
+  number -= 1
+  end
+ "HAPPY NEW YEAR!"
 end
 
-def countdown
+def countdown_with_sleep(number)
+  until number == 0 
+  puts "#{number} SECOND(S)!"
+  > sleep "{5} SECONDS"
+  number -= 1
+  end
+ "HAPPY NEW YEAR!"
 end
